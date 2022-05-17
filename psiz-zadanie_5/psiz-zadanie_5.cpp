fdbb635e8e678813375034e4268a1190fc0f6452
Adam-Pacholski <adam.pacholski1985@gmail.com>
2022-05-17 22:20:02 +0200

    Pierwsze koty za płoty

--- psiz-zadanie_5/psiz-zadanie_5.cpp
+++ psiz-zadanie_5/psiz-zadanie_5.cpp
@@ -1,20 +1,49 @@
-﻿// psiz-zadanie_5.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
-//
-
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
 #include <iostream>
+
+using namespace std;
+
+struct BMP_header
+{
+    char name[2];
+    unsigned int size;
+    int garbage; // nie koniecznie musi byc
+    unsigned int offset;
+}header_bmp;
+
+void openFile() {
+
+
+    FILE* fp = fopen("test.bmp", "rb");
+
+    if (fp == nullptr) {
+        cout << "cos poszlo nie tak";
+        
+    }
+    else {
+        cout << "udalo sie otworzyc plik\n";
+
+        cout << sizeof(BMP_header) << "\n";
+
+        fread(header_bmp.name, 2, 1, fp);
+        fread(&header_bmp.size, 3 * sizeof(int), 1, fp);
+        //fread(&header, sizeof(struct BMP_header), 1, fp);
+        cout << "Pierwsze dwa znaki :" << header_bmp.name[0] << header_bmp.name[1]<<"\n";
+        cout << "Rozmiar pkiku: " << header_bmp.size << "\n";
+
+         fclose(fp);
+    }
+  
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    
+    openFile();
+
+    return 0;
 }
 
-// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
-// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
 
-// Porady dotyczące rozpoczynania pracy:
-//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
-//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
-//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
-//   4. Użyj okna Lista błędów, aby zobaczyć błędy
-//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
-//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
